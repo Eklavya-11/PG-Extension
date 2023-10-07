f5d293e2b211167fb4ca4ecf617512c3fc0e2d8a
Eklavya11 <117552685+Eklavya-11@users.noreply.github.com>
2023-10-07 15:06:53 +0200

    Update db.js

--- interface/pages/db.js
+++ interface/pages/db.js
@@ -1,1 +1,22 @@
-ss
+/* Saving the settings to the localStorage to access them in modules */
+
+// Themes.html
+document.addEventListener('DOMContentLoaded', function () {
+
+var dropdownItems = document.querySelectorAll('.dropdown-item');
+
+dropdownItems.forEach(function(item) {
+  item.addEventListener('click', function(event) {
+    event.preventDefault();
+    var theme = this.getAttribute('data-theme');
+    saveTheme(theme);
+  });
+});
+
+function saveTheme(themeName){
+    chrome.storage.local.set({ 'theme': themeName});
+}
+// Team.html - custom teams layouts
+
+
+}, false);
