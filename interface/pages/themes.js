9c5c4cd9aaf2adc5109c82d6442540a9d4f4cb1d
Eklavya11 <117552685+Eklavya-11@users.noreply.github.com>
2023-10-07 15:07:20 +0200

    Update themes.js

--- interface/pages/themes.js
+++ interface/pages/themes.js
@@ -1,1 +1,72 @@
-dd
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Pokemongod Extension</title>
+	  <!-- CSS & JS only -->
+	<link rel="stylesheet" href="/library/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/interface/main.css">
+	<script type="text/javascript" src="/interface/pages/db.js"></script>
+	<script src="/library/bootstrap/bootstrap.bundle.min.js"></script>
+
+</head>
+<body>
+	<div class="card-container" id="card-container">
+		<div class="card" id="card">
+			<div class="card-header" id="card-header">
+				<h1 id="welcome-msg">Welcome Trainer</h1>
+				<img src="/assets/profiles/redtrainer.jpeg" alt="Logo">
+			</div>
+			<div class="card-content" id="card-content">
+<!-- start -->
+<div class="container">
+	<div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+
+		<div class="dropdown">
+			<button class="btn btn-dark dropdown-toggle" type="button" data-bs-toggle="dropdown">Select Theme
+			  <span class="caret"></span>
+			</button>
+			<ul class="dropdown-menu" style="max-height: 200px; overflow-y: auto;">
+			  <li><a class="dropdown-item" href="#" data-theme="BlackRed">BlackRed</a></li>
+			  <li><a class="dropdown-item" href="#" data-theme="BlueTheme">Blue</a></li>
+			  <li><a class="dropdown-item" href="#" data-theme="CoffeeTheme">Coffee</a></li>
+			  <li><a class="dropdown-item" href="#" data-theme="ElitePizani">Elite Pizani</a></li>
+			  <li><a class="dropdown-item" href="#" data-theme="GreenTheme">Green</a></li>
+			  <li><a class="dropdown-item" href="#" data-theme="LightTheme">Light</a></li>
+			  <li><a class="dropdown-item" href="#" data-theme="PinkTheme">Pink</a></li>
+			  <li><a class="dropdown-item" href="#" data-theme="EliteCamckee">Elite Camckee</a></li>
+			  <li><hr class="dropdown-divider"></li>
+			  <li><a class="dropdown-item" href="#" data-theme="None">None</a></li>
+			</ul>
+		  </div>
+</div>
+
+
+</div>
+
+	  <!-- <div class="text-end">
+		<button id="" type="button" class="btn btn-outline-light me-2">Update preferences</button>
+	  </div> -->
+
+	</div>
+  </div>
+</header>
+
+<!--  end -->
+
+			</div>
+			<div class="card-footer" id="card-footer">
+				<nav class="bottom-nav">
+					<ul>
+						<li><a href="/interface/main.html" id="hunting-link">Hunting</a></li>
+						<li><a href="/interface/pages/themes.html" id="">Themes</a></li>
+						<li><a href="/interface/pages/team.html" id="team-link">Team</a></li>
+						<li><a href="#" id="team-link">Developer: Eklavya11</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</div>
+	
+</body>
+</html>
